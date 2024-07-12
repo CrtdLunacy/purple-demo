7b935b7d417be771f06bde751e76e6076eba5cd2
Vyacheslav Frolov <crtdlunacy@gmail.com>
2024-07-12 18:20:45 +0300

    update converter for i/o

--- 1_converter/main.go
+++ 1_converter/main.go
@@ -9,4 +9,25 @@ func main() {
 	usdToRub := 87.0
 	eurToRub := usdToRub / usdToEur
 	fmt.Print(eurToRub)
+
+	initCurrency, targetCurrency, sumForConvertation := userInputData()
+	fmt.Print(initCurrency, targetCurrency, sumForConvertation)
+}
+
+func userInputData() (string, string, float64) {
+	var initCurrency string
+	var targetCurrency string
+	var sumForConvertation float64
+	fmt.Print("Введите валюту, из которой хотите конвертировать (USD, RUB, EURO)")
+	fmt.Scan(&initCurrency)
+	fmt.Print("Введите валюту, которую хотите получить (USD, RUB, EURO)")
+	fmt.Scan(&targetCurrency)
+	fmt.Print("Введите сумму, которую хотите рассчитать")
+	fmt.Scan(&sumForConvertation)
+
+	return initCurrency, targetCurrency, sumForConvertation
+}
+
+func currencyConverter(initValue string, targetValue string, sum float64) {
+
 }
