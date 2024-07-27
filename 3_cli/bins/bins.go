d9ca9d8da34e8cc7b8504e4daf086dab43214cfe
Vyacheslav Frolov <crtdlunacy@gmail.com>
2024-07-27 09:19:55 +0300

    3_last

--- 3_cli/bins/bins.go
+++ 3_cli/bins/bins.go
@@ -1,27 +1,43 @@
 package bins
 
 import (
+	"encoding/json"
+	"errors"
 	"time"
 )
 
 type Bin struct {
-	id        string
-	private   bool
-	createdAt time.Time
-	name      string
+	Id        int32     `json:"id"`
+	Private   bool      `json:"private"`
+	CreatedAt time.Time `json:"createdAt"`
+	Name      string    `json:"name"`
 }
 
 type BinList struct {
 	binList []Bin
 }
 
-func NewBin(id, name string, private bool) Bin {
-	return Bin{
-		id:        id,
-		private:   private,
-		createdAt: time.Now(),
-		name:      name,
+func NewBin(id int32, name string, private bool) (*Bin, error) {
+	if name == "" {
+		return nil, errors.New("INVALID_NAME")
 	}
+
+	newBin := &Bin{
+		Id:        id,
+		Private:   private,
+		CreatedAt: time.Now(),
+		Name:      name,
+	}
+
+	return newBin, nil
+}
+
+func (bin *Bin) BinToBytes() ([]byte, error) {
+	file, err := json.Marshal(bin)
+	if err != nil {
+		return nil, err
+	}
+	return file, nil
 }
 
 func NewBinList() BinList {
