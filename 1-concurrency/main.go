9e9b4cf1dea12ca9e1785145815d677cde2957f0
Vyacheslav Frolov <crtdlunacy@gmail.com>
2024-10-31 20:01:06 +0300

    first homework advanced
    Первая работка по горутинам

---
+++ 1-concurrency/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+)
+
+func main() {
+
+	powCh := make(chan []int, 1)
+	resCh := make(chan []int, 1)
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	go func() {
+
+		defer wg.Done()
+		var transferSlice []int
+		count := 10
+		for i := 0; i < count; i++ {
+			transferSlice = append(transferSlice, rand.Intn(101))
+		}
+
+		powCh <- transferSlice
+	}()
+
+	go func() {
+		defer wg.Done()
+		var resultSlice []int
+
+		for _, v := range <-powCh {
+			resultSlice = append(resultSlice, v*v)
+		}
+
+		resCh <- resultSlice
+	}()
+
+	wg.Wait()
+
+	fmt.Println(<-resCh)
+}
